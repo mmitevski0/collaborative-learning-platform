3d2291c0748d0bce46b7f92b3799fd6fe79fe80f
Vladislav Angelovski <angelovski2505@gmail.com>
2025-05-15 12:17:32 +0200

    Remove API key from firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,26 +7,17 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: import.meta.env.VITE_FIREBASE_APP_ID,
-//     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-// };
 const firebaseConfig = {
-    apiKey: "AIzaSyDHdysyy1I-Q-HOBvSeNRelWUCVJefe9zA",
-    authDomain: "uiktp-team-project.firebaseapp.com",
-    projectId: "uiktp-team-project",
-    storageBucket: "uiktp-team-project.firebasestorage.app",
-    messagingSenderId: "341574532253",
-    appId: "1:341574532253:web:5add2eb46e05741eaca874",
-    measurementId: "G-EW8RLC6T07"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+getAnalytics(app);
 export const db = getFirestore(app);
